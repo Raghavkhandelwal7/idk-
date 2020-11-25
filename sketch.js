44e8fbfc9f2c0080d75c02e68949de5a21c42ddd
Raghavkhandelwal7 <71634967+Raghavkhandelwal7@users.noreply.github.com>
2020-11-25 18:28:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var dog,happayDog,database,foodStock,foodS,dogIMG,happyDogIMG;
+
+function preload()
+{
+ dogIMG=loadImage("Dog.png");
+  happayDogIMG=loadImage("happydog.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog=createSprite(200,100,120,20);
+  dog.addImage(dogIMG);
+  database=firebase.database();
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+}
+
+
+function draw() {  
+  background(46,139,87);
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happayDogIMG);
+    foodS=foodS-1;
+  }  
+  drawSprites();
+
+}
+function readStock(data){
+foodS=data.val;
+}
+function writeStock(x){
+
+  if(x=0){
+    x=0
+  }
+  else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
